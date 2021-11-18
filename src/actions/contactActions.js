0f30f10329de404cdcfc2871aeb0ce6e57ac9168
Chanteng <nanaquamichanteng@gmail.com>
2021-11-18 07:32:32 -0800

    added editing and retrieving from firestore

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -1,12 +1,11 @@
 export const addContacts = (contacts) => {
-    return (dispatch, state, {getFirestore}) => {
+  return (dispatch, state, { getFirestore }) => {
     getFirestore()
-    .collection("contacts")
-    .add(contacts)
-    .then(()=>{});
-    };
+      .collection("contacts")
+      .add(contacts)
+      .then(() => {});
+  };
 };
-
 
 // export const addContacts = (newContacts) => {
 // return {
@@ -15,37 +14,54 @@ export const addContacts = (contacts) => {
 // };
 // };
 
-export const editContatcts = (id, updatedContact) => {
-    return {
-        type: "EDIT_CONTACT",
-        payload: {id, updatedContact}
-    }
+export default editContacts = (id, updatedContact) => {
+  return (dispatch, state, { getFirestore }) => {
+    getFirestore()
+      .collection("contacts")
+      .doc(id)
+      .update(updatedContact)
+      .then(() => {});
+  };
 };
 
+// export const editContatcts = (id, updatedContact) => {
+//     return {
+//         type: "EDIT_CONTACT",
+//         payload: {id, updatedContact}
+//     }
+// };
 
-export const deleteContacts = (id) => {
-    return {
-        type: "DELETE_CONTACT",
-        payload: id
-    }
+export default deleteContacts = (id) => {
+    return (dispatch, state, { getFirestore }) => {
+        getFirestore()
+          .collection("contacts")
+          .doc(id)
+          .delete()
+          .then(() => {});
+      };
 }
 
-
+// export const deleteContacts = (id) => {
+//   return {
+//     type: "DELETE_CONTACT",
+//     payload: id,
+//   };
+// };
 
 export const getAllContact = () => {
-    return (dispatch, state, {getFirestore}) => {
-        getFirestore()
-        .collection ("contacts")
-        .onSnapshot (
-            (snapshot) => {
-                let contacts = [];
-                snapshot.forEach((doc) => {
-                    contacts.push(doc.data());
-                });
-                console.log(contacts);
-                dispatch({type: "SET_ALL_CONTACTS", payload: contacts})
-            },
-            (error) => {}
-        )
-    }
-}
+  return (dispatch, state, { getFirestore }) => {
+    getFirestore()
+      .collection("contacts")
+      .onSnapshot(
+        (snapshot) => {
+          let contacts = [];
+          snapshot.forEach((doc) => {
+            contacts.push({ ...doc.data(), id: doc.id });
+          });
+          console.log(contacts);
+          dispatch({ type: "SET_ALL_CONTACTS", payload: contacts });
+        },
+        (error) => {}
+      );
+  };
+};
